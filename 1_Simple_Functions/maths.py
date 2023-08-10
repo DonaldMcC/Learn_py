3386ba12b86315aaeb276fe30692dec4dfb6876e
Donald <DonaldMcC@users.noreply.github.com>
2023-08-10 22:15:14 +0100

    start on very simple function

---
+++ 1_Simple_Functions/maths.py
@@ -0,0 +1,23 @@
+# This will introduce the simplest of maths functions as everyone understands
+# them.  Obviously there is no actual requirement to create these functions
+# as python has them and many others built in.
+# However it should provide the key features of functions in that they accept
+# parameters and return a value.
+# We will keep the add function super-clean but improve further ones with
+# docstrings and options
+
+def func_add(a, b):
+    return a + b
+    
+    
+def func_subtract(a, b):
+    return a - b
+
+    
+def func_multiply(a, b):
+    return a * b
+
+    
+def func_divide(a, b):
+    return a / b
+    
