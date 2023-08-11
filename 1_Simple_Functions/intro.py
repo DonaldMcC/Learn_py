203b976549fe76799ad1dd52ea797ae7e96295f4
Donald McClymont <Donaldm2020@gmail.com>
2023-08-11 09:39:08 +0100

    Merge intro

---
+++ 1_Simple_Functions/intro.py
@@ -0,0 +1,11 @@
+#  This is supposed to be about the simplest examples of python contstructs I can think of
+#  to best demo use of language
+
+
+def for_loop():
+    for x in range(10):
+        print(x)
+
+if __name__ == '__main__':
+    for_loop()
+
